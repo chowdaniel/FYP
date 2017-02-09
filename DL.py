4772a045c3693bb84e30c1aa62a4065b301b64eb
chowdaniel <danielchow798@gmail.com>
2017-02-09 17:54:38 +0700

    Added historical prices for SnP500

---
+++ DL.py
@@ -0,0 +1,19 @@
+import pandas_datareader.data as web
+import datetime
+import os
+
+a = open("Stocks.txt","r")
+
+start = datetime.datetime(2015,1,1)
+end = datetime.datetime(2017,1,1)
+
+for line in a:
+	name = line.replace("\n","")
+
+	try:
+		d = web.DataReader(name,"yahoo",start,end)
+		path = os.path.join("Data",name + ".csv")
+
+		d.to_csv(path,columns= ["Adj Close"])
+	except:
+		print name + "Error"

c849e6d6cfc7ec029745e5d2713022abac33516a
chowdaniel <danielchow798@gmail.com>
2017-02-12 20:15:20 +0700

    Added deep neural network for replication of SnP500

--- Model.py
+++ Model.py
@@ -11,41 +11,114 @@ HIDDEN_LAYER_DIM = 200
 
 
 #Begin Code
-model = Sequential()
-
 imported_data = pandas.read_csv(FILENAME,header=0,index_col=0)
 data = imported_data.as_matrix()
 
 data = numpy.log(data)
 #data = numpy.diff(data,axis=0)
 
-#print data
-
 data_length = data.shape[1]
 
-#Build and Fit Model
-model.add(Dense(output_dim=HIDDEN_LAYER_DIM,input_dim=data_length,activation="relu"))
-model.add(Dense(output_dim=data_length,activation="relu"))
-
-model.compile(optimizer="sgd",loss="mse",metrics=["accuracy"])
-
-model.fit(data,data,batch_size=10,nb_epoch=20,validation_split=0)
-
-#Calculate Error for each Symbol
-y_pred = model.predict(data)
+#print data
 
 
-diff = data - y_pred
-SSE = numpy.sum(numpy.square(diff),axis=0)
+def AutoEncoder():
+	#Build and Fit Model
+	model = Sequential()
 
-txt = ""
+	model.add(Dense(output_dim=HIDDEN_LAYER_DIM,input_dim=data_length,activation="relu"))
+	model.add(Dense(output_dim=data_length,activation="relu"))
 
-for element in SSE:
-	txt = txt + str(element.item())
-	txt = txt + ","
+	model.compile(optimizer="sgd",loss="mse",metrics=["accuracy"])
 
-txt += "\n"
+	model.fit(data,data,batch_size=10,nb_epoch=20,validation_split=0)
 
-out = open("Results.txt","a")
-out.write(txt)
-out.close()
+	#Calculate Error for each Symbol
+	y_pred = model.predict(data)
+
+
+	diff = data - y_pred
+	SSE = numpy.sum(numpy.square(diff),axis=0)
+
+	txt = ""
+
+	for element in SSE:
+		txt = txt + str(element.item())
+		txt = txt + ","
+
+	txt = txt[:-1]
+	txt += "\n"
+
+	out = open("Results.csv","a")
+	out.write(txt)
+	out.close()
+
+def parseResults():
+	FILENAME = "Merged.csv"
+
+	imported_data = pandas.read_csv(FILENAME,header=0,index_col=0)
+	stocks = list(imported_data)
+
+	my_data = numpy.genfromtxt("Results.csv", delimiter=',')
+	s = numpy.sum(my_data,axis=0)
+
+	res = []
+
+	print "%d rows of data found" % len(s)
+
+	for i in range(len(s)):
+		temp = [stocks[i],s[i].item()]
+		res.append(temp)
+
+	res = sorted(res,key=lambda x:x[1])
+
+	return res
+
+def indexWeights(s):
+	#s is the results from parseResults
+
+	#Import data for S&P500
+	stocks = ["^GSPC"]
+
+	#Take the 10 stocks with the lowest error
+	for i in range(10):
+		stocks.append(s[i][0])
+
+	#Build stock historical data into DataFrame
+	data = pandas.DataFrame()
+
+	for stock in stocks:
+		path = os.path.join("Data",stock + ".csv")
+	
+		d = pandas.read_csv(path,header=0,index_col=0)
+		d.columns = [stock]
+
+		data = data.merge(d,how="outer",left_index=True,right_index=True)
+
+	#Extract X and Y as numpy arrays
+	X = numpy.log(data.as_matrix(columns=stocks[1:]))
+	Y = numpy.log(data.as_matrix(columns=stocks[0:1]))
+
+	#Build Deep Network
+	model = Sequential()
+
+	model.add(Dense(output_dim=4,input_dim=X.shape[1],activation="relu"))
+	model.add(Dense(output_dim=2,activation="relu"))
+	model.add(Dense(output_dim=1,activation="relu"))
+
+	model.compile(optimizer="sgd",loss="mse",metrics=["accuracy"])
+
+	model.fit(X,Y,batch_size=10,nb_epoch=20,validation_split=0)
+	
+
+
+if __name__ == "__main__":
+	#for i in range(10):
+	#	AutoEncoder()
+
+	res = parseResults()
+	indexWeights(res)
+
+
+
+

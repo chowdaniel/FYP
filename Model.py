b918b685e42f97ecc3aaa77fc7648a55dd5c3f22
chowdaniel <danielchow798@gmail.com>
2017-02-16 09:15:32 +0700

    X

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense,Activation
+from keras.utils.visualize_util import plot
 
 import pandas
 import numpy
@@ -33,6 +34,8 @@ def AutoEncoder():
 
 	model.fit(data,data,batch_size=10,nb_epoch=20,validation_split=0)
 
+	plot(model,to_file="Encoder.png",show_shapes=True)
+	return
 	#Calculate Error for each Symbol
 	y_pred = model.predict(data)
 
@@ -84,6 +87,9 @@ def indexWeights(s):
 	for i in range(10):
 		stocks.append(s[i][0])
 
+	for i in range(-10,0):
+		stocks.append(s[i][0])
+
 	#Build stock historical data into DataFrame
 	data = pandas.DataFrame()
 
@@ -96,25 +102,43 @@ def indexWeights(s):
 		data = data.merge(d,how="outer",left_index=True,right_index=True)
 
 	#Extract X and Y as numpy arrays
-	X = numpy.log(data.as_matrix(columns=stocks[1:]))
-	Y = numpy.log(data.as_matrix(columns=stocks[0:1]))
+	X = data.as_matrix(columns=stocks[1:])
+	Y = data.as_matrix(columns=stocks[0:1])
+
+	#log prices
+	X = numpy.log(X)
+	Y = numpy.log(Y)
+
+	#log returns
+	X = numpy.diff(X,axis=0)
+	Y = numpy.diff(Y,axis=0)
 
 	#Build Deep Network
 	model = Sequential()
 
-	model.add(Dense(output_dim=4,input_dim=X.shape[1],activation="relu"))
-	model.add(Dense(output_dim=2,activation="relu"))
-	model.add(Dense(output_dim=1,activation="relu"))
+	model.add(Dense(output_dim=4,input_dim=X.shape[1]))
+	model.add(Dense(output_dim=2))
+	model.add(Dense(output_dim=1))
 
 	model.compile(optimizer="sgd",loss="mse",metrics=["accuracy"])
 
 	model.fit(X,Y,batch_size=10,nb_epoch=20,validation_split=0)
-	
 
+	plot(model,to_file="Model.png",show_shapes=True)
+
+	Y_pred = model.predict(X)
+
+	res = []
+	for i in range(len(Y)):
+		temp = [Y_pred[i].item(),Y[i].item()]
+		res.append(temp)
+
+	print res
 
 if __name__ == "__main__":
-	#for i in range(10):
-	#	AutoEncoder()
+#	for i in range(10):
+#		AutoEncoder()
+	AutoEncoder()
 
 	res = parseResults()
 	indexWeights(res)

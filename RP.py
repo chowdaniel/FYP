385b0205a1f2df6f5d6bf452c7ef1d6d85f2f99a
chowdaniel <danielchow798@gmail.com>
2017-04-01 21:33:49 +0800

    Tweaked to make RP more flexible

--- RP.py
+++ RP.py
@@ -6,117 +6,97 @@ import pandas
 import numpy
 import os
 
-def Replicating(sample,validation,nLow,nHigh):
+def Replicating(sample,validation,N):
 
-	#Import list of stocks sorted by increasing SSE
-	symbols = open("Portfolio.txt","r")
+	s_res = pandas.DataFrame(index=sample.index[1:])
+	v_res = pandas.DataFrame(index=validation.index[1:])
 
-	stocks = []
-	for symbol in symbols:
-		if symbol != "\n":
-			stocks.append(symbol.replace("\n",""))
-	symbols.close()
-	
-	#Select the stocks used for replicating portfolio
-	chosen_stocks = []
-	#10 Stocks with most communal info
-	for i in range(nLow):
-		chosen_stocks.append(stocks[i])
-	#n stocks with least communal info
-	for i in range(nHigh):
-		chosen_stocks.append(stocks[-i-1])
-	chosen_stocks.append("^GSPC")
+	s_res["^GSPC"] = numpy.diff(numpy.log(sample.as_matrix(columns=["^GSPC"])),axis=0)
+	v_res["^GSPC"] = numpy.diff(numpy.log(validation.as_matrix(columns=["^GSPC"])),axis=0)
 
+	for n in N:
+		#Import list of stocks sorted by increasing SSE
+		symbols = open("Portfolio.txt","r")
 
-#Calibration Phase====================================================
-
-	#Import data for stocks
-	data = pandas.DataFrame()
-
-	for stock in chosen_stocks:
-		prices = sample[stock]
-		data[stock] = prices
-
-	#Extract X and Y as numpy arrays
-	X = data.as_matrix(columns=chosen_stocks[0:-1])
-	Y = data.as_matrix(columns=chosen_stocks[-1:])
-
-	#log prices
-	X = numpy.log(X)
-	Y = numpy.log(Y)
-
-	#log returns
-	X = numpy.diff(X,axis=0)
-	Y = numpy.diff(Y,axis=0)
-
-	#Build Deep Network
-	model = Sequential()
-
-	model.add(Dense(output_dim=4,input_dim=X.shape[1],activation="relu"))
-	model.add(Dense(output_dim=2,activation="relu"))
-	model.add(Dense(output_dim=1,activation="relu"))
-
-	opt = Adam(lr=0.001)
-
-	model.compile(optimizer=opt,loss="mse",metrics=["accuracy"])
-
-	model.fit(X,Y,batch_size=40,nb_epoch=100,validation_split=0,verbose=2)
-
-	Y_pred = model.predict(X)
-
-	output = open("CalibrationResults" + str(n) + ".csv","w")
-	output.write("Date,SnP Return," + str(n) + "\n")
-
-	dates = data.index
-
-	for i in range(len(Y)):
-		output.write(dates[i+1])
-		output.write(",")
-		output.write(str(Y[i].item()))
-		output.write(",")
-		output.write(str(Y_pred[i].item()))
-		output.write("\n")
+		stocks = []
+		for symbol in symbols:
+			if symbol != "\n":
+				stocks.append(symbol.replace("\n",""))
+		symbols.close()
 		
-	output.close()
+		#Select the stocks used for replicating portfolio
+		chosen_stocks = []
+		#10 Stocks with most communal info
+		for i in range(n[0]):
+			chosen_stocks.append(stocks[i])
+		#n stocks with least communal info
+		for i in range(n[1]):
+			chosen_stocks.append(stocks[-i-1])
+		chosen_stocks.append("^GSPC")
 
 
-#Start of Validation Phase====================================================
+	#Calibration Phase====================================================
 
-	#Import data for stocks
-	data = pandas.DataFrame()
+		#Import data for stocks
+		data = pandas.DataFrame()
 
-	for stock in chosen_stocks:
-		prices = validation[stock]
-		data[stock] = prices
+		for stock in chosen_stocks:
+			prices = sample[stock]
+			data[stock] = prices
 
-	#Extract X and Y as numpy arrays
-	X = data.as_matrix(columns=chosen_stocks[0:-1])
-	Y = data.as_matrix(columns=chosen_stocks[-1:])
+		#Extract X and Y as numpy arrays
+		X = data.as_matrix(columns=chosen_stocks[0:-1])
+		Y = data.as_matrix(columns=chosen_stocks[-1:])
 
-	#log prices
-	X = numpy.log(X)
-	Y = numpy.log(Y)
+		#log prices
+		X = numpy.log(X)
+		Y = numpy.log(Y)
 
-	#log returns
-	X = numpy.diff(X,axis=0)
-	Y = numpy.diff(Y,axis=0)
+		#log returns
+		X = numpy.diff(X,axis=0)
+		Y = numpy.diff(Y,axis=0)
 
-	Y_pred = model.predict(X)
+		#Build Deep Network
+		model = Sequential()
 
-	output = open("ValidationResults" + str(n) + ".csv","w")
-	output.write("Date,SnP Return," + str(n) + "\n")
+		model.add(Dense(output_dim=4,input_dim=X.shape[1],activation="relu"))
+		model.add(Dense(output_dim=2,activation="relu"))
+		model.add(Dense(output_dim=1,activation="relu"))
 
-	dates = data.index
+		opt = Adam(lr=0.001)
+		model.compile(optimizer=opt,loss="mse",metrics=["accuracy"])
 
-	for i in range(len(Y)):
-		output.write(dates[i+1])
-		output.write(",")
-		output.write(str(Y[i].item()))
-		output.write(",")
-		output.write(str(Y_pred[i].item()))
-		output.write("\n")
-		
-	output.close()
+		model.fit(X,Y,batch_size=40,nb_epoch=100,validation_split=0,verbose=2)
+
+		Y_pred = model.predict(X)
+		s_res[n] = Y_pred
+
+	#Validation Phase====================================================
+
+		#Import data for stocks
+		data = pandas.DataFrame()
+
+		for stock in chosen_stocks:
+			prices = validation[stock]
+			data[stock] = prices
+
+		#Extract X and Y as numpy arrays
+		X = data.as_matrix(columns=chosen_stocks[0:-1])
+		Y = data.as_matrix(columns=chosen_stocks[-1:])
+
+		#log prices
+		X = numpy.log(X)
+		Y = numpy.log(Y)
+
+		#log returns
+		X = numpy.diff(X,axis=0)
+		Y = numpy.diff(Y,axis=0)
+
+		Y_pred = model.predict(X)
+		v_res[n] = Y_pred
+
+	print s_res
+	print v_res
 
 
 if __name__ == "__main__":
@@ -127,5 +107,5 @@ if __name__ == "__main__":
 	sample = imported_data.loc["2014-01-02":"2015-12-31"]
 	validation = imported_data.loc["2016-01-04":"2016-12-30"]
 
-	Replicating(sample,validation,10,10)
+	Replicating(sample,validation,[(10,10)])
 

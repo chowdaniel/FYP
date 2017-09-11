a88ce9b174caa505ef2277496c226657624408c5
Daniel <danielchow798@gmail.com>
2017-09-11 17:26:53 +0800

    Create folders for DQN

---
+++ 3.2/DeepQ.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+class DeepQ():
+    def __init__(self,model):
+        self.model = model
+        
+        
+    def compile(self):
+        
+        
+    def fit(self):
+        
+        
+    def evaluate(self):
+        pass
+    
+    
+    
+    
